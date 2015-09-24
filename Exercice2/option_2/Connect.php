6c258dbf22ff073924bb227bcec474ec12f2b6b2
System Administrator <root@Texerie.local>
2015-09-24 14:14:36 +0200

    option_2

---
+++ Exercice2/option_2/Connect.php
@@ -0,0 +1,31 @@
+<?php
+include 'vars.php';
+
+$connect = new Connect;
+
+	if($_GET['action'] == 0)
+		$connect->login($_POST['login'], $users);
+
+	elseif($_GET['action'] == 1)
+		$connect->logout();
+
+	header("Location: connection.php");
+
+class Connect
+{
+	//Verification du login(Connection)
+	public function login($log, $users)
+	{
+		foreach ($users as $key_user => $value_user) {
+			if($key_user == $log){
+				$_SESSION['Profil'] = $value_user;
+			}
+		}
+	}
+
+	//Deconection
+	public function logout()
+	{
+		unset($_SESSION['Profil']);
+	}
+}
